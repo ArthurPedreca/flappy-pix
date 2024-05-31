bd294b1892bd3a1a1f10745b154d7f511f1c1ed4
Pedreca <arthurpedreca@gmail.com>
2024-05-31 11:20:11 -0300

    projeto do jogo flappy bird (completo)

---
+++ Assets/Scripts/Obstaculo.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculo : MonoBehaviour
+{
+    [SerializeField]
+    private float velocidade = 0.6f;
+
+    [SerializeField]
+    private float variacaoY;
+    private void Awake()
+    {
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoY,variacaoY));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
+        
+    }
+}
